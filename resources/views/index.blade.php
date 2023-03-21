5e4577c8063a23ca28abdae6df4db68b2cb20efd
Perandres8 <77155860+Perandres8@users.noreply.github.com>
2023-03-21 13:20:56 +0100

    detalles index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,38 +12,33 @@
         <script src="https://cdn.tailwindcss.com"></script>
         <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
 
-        <script src='https://api.tiles.mapbox.com/mapbox-gl-js/v0.49.0/mapbox-gl.js'></script>
-    <link href='https://api.tiles.mapbox.com/mapbox-gl-js/v0.49.0/mapbox-gl.css' rel='stylesheet' />
-	<script src='https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v2.3.0/mapbox-gl-geocoder.min.js'></script>
-    <link rel='stylesheet' href='https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v2.3.0/mapbox-gl-geocoder.css' type='text/css' />
-
     </head>
 
-        <!--animacion underline-->
-        <style>
-            .link-underline {
-                border-bottom-width: 0;
-                background-image: linear-gradient(transparent, transparent), linear-gradient(#fff, #fff);
-                background-size: 0 3px;
-                background-position: 0 100%;
-                background-repeat: no-repeat;
-                transition: .5s ease-in-out;
-            }
+    <!--animacion-->
+    <style>
+        .link-underline {
+            border-bottom-width: 0;
+            background-image: linear-gradient(transparent, transparent), linear-gradient(#fff, #fff);
+            background-size: 0 3px;
+            background-position: 0 100%;
+            background-repeat: no-repeat;
+            transition: .5s ease-in-out;
+        }
 
-            .link-underline-orange {
-                background-image: linear-gradient(transparent, transparent), linear-gradient(rgb(251 146 60 / 0.75), rgb(251 146 60 / 0.75));
-            }
+        .link-underline-orange {
+            background-image: linear-gradient(transparent, transparent), linear-gradient(rgb(251 146 60 / 0.75), rgb(251 146 60 / 0.75));
+        }
 
-            .link-underline:hover {
-                background-size: 100% 3px;
-                background-position: 0 100%
-            }
+        .link-underline:hover {
+            background-size: 100% 3px;
+            background-position: 0 100%
+        }
 
-            .left-full {
-            left: -100%;
-            }
+        .left-full {
+        left: -100%;
+        }
 
-        </style>
+    </style>
 
     <body class="bg-[url('assets/foto.jpg')] bg-no-repeat bg-fixed">
 
@@ -82,60 +77,73 @@
 
         <div class="container flex flex-wrap items-start mt-8 mx-auto">
 
-          <div class="w-full pl-5 lg:pl-2 mb-4 mt-4">
-            <h1 class="text-3xl font-extrabold text-white text-center"> SERVICIOS DESTACADOS </h1>
-          </div>
+            <div class="w-full pl-5 lg:pl-2 mb-4 mt-4">
+                <h1 class="text-3xl font-extrabold text-white text-center"> SERVICIOS DESTACADOS </h1>
+            </div>
 
-          <div class="w-full mx-auto md:w-1/2 lg:w-1/4 pl-5 pr-5 mb-5 lg:pl-2 lg:pr-2 mr-2">
+            <div class="w-full mx-auto md:w-1/2 lg:w-1/4 pl-5 pr-5 mb-5 lg:pl-2 lg:pr-2 mr-2">
 
-            <div class="bg-gray-600 rounded-lg m-h-64 p-2 transform hover:-translate-y-2 hover:shadow-xl transition duration-300">
+                <a href="/fuelStation">
 
-                <figure class="mb-2">
-                    <img src="https://cdn-icons-png.flaticon.com/512/8940/8940365.png " alt="" class="h-64 ml-auto mr-auto" />
-                </figure>
+                    <div class="bg-gray-600 rounded-lg m-h-64 p-2 transform hover:-translate-y-2 hover:shadow-xl transition duration-300">
 
-                <div class="rounded-lg p-4 flex flex-col">
-                    <div>
-                        <h5 class="text-2xl font-bold text-white leading-none text-center"> Estaciones de Servicio </h5>
-                        <p class="mt-4 text-white text-justify">
-                            Establecimientos dedicados a la venta al público de productos derivados del petróleo a granel y mediante surtidores. 
-                            Generalmente se ubican en puntos convenientes de zonas urbanas y próximas a las carreteras, y en ellas, 
-                            es posible parar y repostar el combustible de los vehículos, además de regular la presión de los neumáticos, 
-                            el agua de refrigeración u otros elementos mecánicos.
-                        </p>
+                        <figure class="mb-2"><img src="https://cdn-icons-png.flaticon.com/512/8940/8940365.png " alt="" class="h-64 ml-auto mr-auto" /></figure>
+
+                        <div class="rounded-lg p-4 flex flex-col">
+
+                            <div>
+
+                                <h5 class="text-2xl font-bold text-white leading-none text-center"> Estaciones de Servicio </h5>
+
+                                <p class="mt-4 text-white text-justify">
+                                    Establecimientos dedicados a la venta al público de productos derivados del petróleo a granel y mediante surtidores. 
+                                    Generalmente se ubican en puntos convenientes de zonas urbanas y próximas a las carreteras, y en ellas, 
+                                    es posible parar y repostar el combustible de los vehículos, además de regular la presión de los neumáticos, 
+                                    el agua de refrigeración u otros elementos mecánicos.
+                                </p>
+
+                            </div>
+
+                        </div>
+
                     </div>
-                </div>
+
+                </a>
 
             </div>
 
-          </div>
+            <div class="w-full mx-auto md:w-1/2 lg:w-1/4 pl-5 pr-5 mb-5 lg:pl-2 lg:pr-2 ml-2">
 
-          <div class="w-full mx-auto md:w-1/2 lg:w-1/4 pl-5 pr-5 mb-5 lg:pl-2 lg:pr-2 ml-2">
+                <a href="/maritimePole">
 
-            <div class="bg-gray-600 rounded-lg m-h-64 p-2 transform hover:-translate-y-2 hover:shadow-xl transition duration-300">
+                    <div class="bg-gray-600 rounded-lg m-h-64 p-2 transform hover:-translate-y-2 hover:shadow-xl transition duration-300">
 
-              <figure class="mb-2">
-              <img src="https://cdn-icons-png.flaticon.com/512/8552/8552053.png" alt="" class="h-64 ml-auto mr-auto" />
-              </figure>
+                        <figure class="mb-2"><img src="https://cdn-icons-png.flaticon.com/512/8552/8552053.png" alt="" class="h-64 ml-auto mr-auto" /></figure>
 
-              <div class="rounded-lg p-4 flex flex-col">
+                        <div class="rounded-lg p-4 flex flex-col">
 
-                <div>
-                  <h5 class="text-2xl font-bold text-white leading-none text-center"> Postes Maritimos </h5>
-                  <p class="mt-4 text-white text-justify">
-                        Puntos de suministro de combustible en puertos y terminales maritimas. 
-                        Estos postes marítimos se utilizan para transferir grandes cantidades de combustible 
-                        desde buques cisterna a tanques de almacenamiento terrestre o para cargar directamente 
-                        combustible en barcos y otras embarcaciones. Suele estar equipados con mangueras y conexiones 
-                        para transferir de forma segura y eficiente del combustible.
-                  </p>
-                </div>
+                            <div>
 
-              </div>
+                                <h5 class="text-2xl font-bold text-white leading-none text-center"> Postes Marítimos </h5>
+
+                                <p class="mt-4 text-white text-justify">
+                                    Puntos de suministro de combustible en puertos y terminales maritimas. 
+                                    Estos postes marítimos se utilizan para transferir grandes cantidades de combustible 
+                                    desde buques cisterna a tanques de almacenamiento terrestre o para cargar directamente 
+                                    combustible en barcos y otras embarcaciones. Suele estar equipados con mangueras y conexiones 
+                                    para transferir de forma segura y eficiente del combustible.
+                                </p>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </a>
 
             </div>
 
-          </div>
 
         </div>
 
@@ -172,12 +180,14 @@
                         </div>
 
                         <div>
+
                             <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Follow us</h2>
 
                             <ul class="text-gray-600 dark:text-gray-400">
                                 <li class="mb-4"><a href="#" class="hover:underline ">Github</a></li>
                                 <li><a href="#" class="hover:underline">Discord</a></li>
                             </ul>
+
                         </div>
 
                         <div>
